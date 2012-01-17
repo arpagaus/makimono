fb44162cf501b1f85c90bc730035054e00e92ac0
Remo Arpagaus <remo.arpagaus@gmx.ch>
2012-01-17 08:22:16 +0100

    Redesigned search list entry and dictionary entry

--- android.jiten/src/android/jiten/activity/HomeActivity.java
+++ android.jiten/src/android/jiten/activity/HomeActivity.java
@@ -3,6 +3,9 @@ package android.jiten.activity;
 import android.jiten.R;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class HomeActivity extends FragmentActivity {
 
@@ -11,5 +14,13 @@ public class HomeActivity extends FragmentActivity {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_home);
+
+		((Button) findViewById(R.id.button_home_search)).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				onSearchRequested();
+			}
+		});
 	}
 }
