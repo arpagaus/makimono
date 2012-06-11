e2a55fd27b04db947acc983d924626da4d4ba7fd
Remo Arpagaus <remo.arpagaus@gmx.ch>
2012-06-11 22:36:51 +0200

    Lint cleanup

--- net.makimono.dictionary/src/net/makimono/dictionary/util/ExternalStorageUtil.java
+++ net.makimono.dictionary/src/net/makimono/dictionary/util/ExternalStorageUtil.java
@@ -2,12 +2,14 @@ package net.makimono.dictionary.util;
 
 import java.io.File;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Build;
 import android.os.Environment;
 
 public class ExternalStorageUtil {
 
+	@SuppressLint("NewApi")
 	public static File getExternalFilesDir(Context context) {
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO) {
 			return context.getExternalFilesDir(null);
