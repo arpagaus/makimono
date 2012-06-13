610ccc4f4c0e40a322c12be81ea0e276c451a443
Remo Arpagaus <remo.arpagaus@gmx.ch>
2012-06-13 21:11:49 +0200

    APK Expansion Files  - Added salt & public key

--- net.makimono.dictionary/src/net/makimono/dictionary/service/ExpansionFileDownloaderService.java
+++ net.makimono.dictionary/src/net/makimono/dictionary/service/ExpansionFileDownloaderService.java
@@ -3,8 +3,8 @@ package net.makimono.dictionary.service;
 import com.google.android.vending.expansion.downloader.impl.DownloaderService;
 
 public class ExpansionFileDownloaderService extends DownloaderService {
-	public static final String BASE64_PUBLIC_KEY = "YourLVLKey"; // TODO
-	public static final byte[] SALT = new byte[] { 1, 42, -12, -1, 54, 98, -100, -12, 43, 2, -8, -4, 9, 5, -106, -107, -33, 45, -1, 84 }; // TODO
+	public static final String BASE64_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwlyDw1iG9oNkfhypwjupjoiuT/TGRWpgCe9wdE8f5M7fqj4RAmli5ZIto5nHgadGFnblZZIkBHxpNhzAivr1Kw+UyEvBeRkbzeJj762xre5BG+6iYm7deyEiVRRoElShnaaJSeWK3uY3zt5KKghcyJbkZeW7XCq1Z+fukViC+14hFSHT8tMHi/qMpjziog1zdS5p1prDmRUHL2HlCuWu0c402f8RWs9EYZ8PifUH1z7vlpDPNTS8Ozne3Z/WcACtv2Qk4eQANAHYv2BFKFrwOJYPzygbtBC/btrD6YNInNO4itYZanOPoITMm2XSuMs+jD3uXrL+9QMsh2LXxtkrZQIDAQAB";
+	public static final byte[] SALT = new byte[] { 121, -30, -115, 52, -71, -15, -50, 14, -128, 9, 87, 80, 78, -82, -57, -115, -110, -110, -70, -92 };
 
 	@Override
 	public String getPublicKey() {
